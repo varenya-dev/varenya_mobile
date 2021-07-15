fe62d8203a6323c2a64c406e5b2a1d8dc12926d7
Varun Khatri <varun.h.khatri@gmail.com>
2021-07-15 17:19:07 +0530

    Services implemented for authentication. Closes #3

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:varenya_mobile/dtos/auth/login_account_dto/login_account_dto.dart';
+import 'package:varenya_mobile/dtos/auth/register_account_dto/register_account_dto.dart';
+import 'package:varenya_mobile/exceptions/auth/user_already_exists_exception.dart';
+import 'package:varenya_mobile/exceptions/auth/user_not_found_exception.dart';
+import 'package:varenya_mobile/exceptions/auth/wrong_password_exception.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+  final Uuid uuid = Uuid();
+
+  /*
+   * Method to register the user with Firebase.
+   * @param registerAccountDto DTO for user registration.
+   */
+  Future<void> registerWithEmailAndPassword(
+    RegisterAccountDto registerAccountDto,
+  ) async {
+    try {
+      // Register the user in firebase.
+      UserCredential userCredential =
+          await this.firebaseAuth.createUserWithEmailAndPassword(
+                email: registerAccountDto.emailAddress,
+                password: registerAccountDto.password,
+              );
+
+      // Fetch the firebase user details from firebase.
+      User? firebaseUser = userCredential.user;
+
+      // Update the user details with name and profile picture.
+      if (firebaseUser != null) {
+        await firebaseUser.updateDisplayName(registerAccountDto.fullName);
+        await firebaseUser.updatePhotoURL(registerAccountDto.imageUrl);
+      }
+    } on FirebaseException catch (error) {
+      // If email is already in use, throw an error.
+      if (error.code == 'email-already-in-use') {
+        throw new UserAlreadyExistsException(
+          message: 'The account already exists for that email.',
+        );
+      }
+    }
+  }
+
+  /*
+   * Method to log the user in the application.
+   * @param loginAccountDto DTO for user login
+   */
+  Future<void> login(LoginAccountDto loginAccountDto) async {
+    try {
+      // Log the user in with firebase using their credentials.
+      await firebaseAuth.signInWithEmailAndPassword(
+        email: loginAccountDto.emailAddress,
+        password: loginAccountDto.password,
+      );
+    } on FirebaseAuthException catch (error) {
+      // Firebase Error: If the user does not exist.
+      if (error.code == 'user-not-found') {
+        throw UserNotFoundException(
+          message: 'No user found for that email.',
+        );
+      }
+      // Firebase Error: If the user has typed the wrong password.
+      else if (error.code == 'wrong-password') {
+        throw WrongPasswordException(
+          message: 'Wrong password provided for the specified user account.',
+        );
+      }
+    }
+  }
+
+  /*
+   * Method to log out from firebase.
+   */
+  Future<void> logOut() async {
+    await this.firebaseAuth.signOut();
+    await this.googleSignIn.signOut();
+  }
+
+  /*
+   * Method to upload image to firebase.
+   * @param imageFile File object for the image itself.
+   */
+  Future<String> uploadImageToFirebase(File imageFile) async {
+    String uid = uuid.v4();
+
+    // Upload image to firebase.
+    await this
+        .firebaseStorage
+        .ref("profilePictures/$uid.png")
+        .putFile(imageFile);
+
+    // Generate a URL for the uploaded image.
+    return await this
+        .firebaseStorage
+        .ref("profilePictures/$uid.png")
+        .getDownloadURL();
+  }
+}
