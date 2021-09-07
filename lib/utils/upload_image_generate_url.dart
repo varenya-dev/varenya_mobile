c325fd92b67cfbcbddb493a4eddef2c406995583
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-09-07 12:13:01 +0530

    Documentation completed. Closes #28

--- lib/utils/upload_image_generate_url.dart
+++ lib/utils/upload_image_generate_url.dart
@@ -3,6 +3,11 @@ import 'dart:io';
 import 'package:uuid/uuid.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+/*
+ * Upload image to firebase storage and return a URL for the same.
+ * @param imageFile File object of the image.
+ * @param folderName Folder where the image is to be uploaded.
+ */
 Future<String> uploadImageAndGenerateUrl(
   File imageFile,
   String folderName,
@@ -10,10 +15,11 @@ Future<String> uploadImageAndGenerateUrl(
   FirebaseStorage firebaseStorage = FirebaseStorage.instance;
   Uuid uuid = new Uuid();
 
+  // Generate a UUID upload the image to storage.
   String imageUuid = uuid.v4();
-
   await firebaseStorage.ref("$folderName/$imageUuid.png").putFile(imageFile);
 
+  // Return the URL for the same.
   return await firebaseStorage
       .ref("$folderName/$imageUuid.png")
       .getDownloadURL();
