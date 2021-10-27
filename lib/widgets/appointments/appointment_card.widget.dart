e0fc7b116c0c86d1262a63e6c9528bb4ffeeaa83
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-27 14:40:03 +0530

    Service implementation for deleting the appointment. Closes #54

--- lib/widgets/appointments/appointment_card.widget.dart
+++ lib/widgets/appointments/appointment_card.widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:varenya_mobile/enum/confirmation_status.enum.dart';
 import 'package:varenya_mobile/models/appointments/patient_appointment_response/patient_appointment_response.model.dart';
+import 'package:varenya_mobile/services/appointment.service.dart';
 
 class AppointmentCard extends StatefulWidget {
   final PatientAppointmentResponse appointment;
@@ -16,31 +18,68 @@ class AppointmentCard extends StatefulWidget {
 }
 
 class _AppointmentCardState extends State<AppointmentCard> {
+  late final AppointmentService _appointmentService;
+
+  @override
+  void initState() {
+    super.initState();
+
+    this._appointmentService =
+        Provider.of<AppointmentService>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Dr ${widget.appointment.doctor.fullName}',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Dr ${widget.appointment.doctor.fullName}',
+                    ),
+                    if (widget.appointment.appointment.status ==
+                        ConfirmationStatus.PENDING)
+                      Text(
+                        widget.appointment.appointment.status
+                            .toString()
+                            .split('.')[1],
+                        textAlign: TextAlign.left,
+                      ),
+                    if (widget.appointment.appointment.status ==
+                        ConfirmationStatus.CONFIRMED)
+                      Text(
+                        'Confirmed for: ${DateFormat.yMd().add_jm().format(
+                              widget.appointment.appointment.scheduledFor
+                                  .toLocal(),
+                            ).toString()}',
+                        textAlign: TextAlign.left,
+                      ),
+                  ],
+                ),
+                PopupMenuButton(
+                  elevation: 40,
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Text("Delete"),
+                      value: 1,
+                    ),
+                  ],
+                  onSelected: (int? value) async {
+                    await this
+                        ._appointmentService
+                        .deleteAppointment(widget.appointment.appointment);
+                  },
+                ),
+              ],
             ),
-            if (widget.appointment.appointment.status ==
-                ConfirmationStatus.PENDING)
-              Text(
-                widget.appointment.appointment.status.toString().split('.')[1],
-                textAlign: TextAlign.left,
-              ),
-            if (widget.appointment.appointment.status ==
-                ConfirmationStatus.CONFIRMED)
-              Text(
-                'Confirmed for: ${DateFormat.yMd().add_jm().format(
-                      widget.appointment.appointment.scheduledFor.toLocal(),
-                    ).toString()}',
-                textAlign: TextAlign.left,
-              ),
           ],
         ),
       ),
