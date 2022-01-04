938194319c5253fde9831e6043af2112caf8bc62
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-04 11:27:59 +0530

    Implementation of a categorized posts page. Closes #93

--- lib/app.dart
+++ lib/app.dart
@@ -11,6 +11,7 @@ import 'package:varenya_mobile/pages/common/splash_page.dart';
 import 'package:varenya_mobile/pages/doctor/doctor_details.page.dart';
 import 'package:varenya_mobile/pages/doctor/doctor_list.page.dart';
 import 'package:varenya_mobile/pages/home_page.dart';
+import 'package:varenya_mobile/pages/post/categorized_posts.page.dart';
 import 'package:varenya_mobile/pages/post/new_posts.page.dart';
 import 'package:varenya_mobile/pages/user/user_update_page.dart';
 
@@ -43,6 +44,7 @@ class App extends StatelessWidget {
         AppointmentList.routeName: (context) => AppointmentList(),
         AppointmentSlots.routeName: (context) => AppointmentSlots(),
         NewPosts.routeName: (context) => NewPosts(),
+        CategorizedPosts.routeName: (context) => CategorizedPosts(),
       },
       home: SplashPage(),
     );
