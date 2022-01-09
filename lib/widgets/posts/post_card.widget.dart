9bd91230b78285481448a520ea320500ca0f533a
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-09 11:11:42 +0530

    Implementation for UI list display for comments and Implementation for UI updating comments form. Closes #107 and Closes #105

--- lib/widgets/posts/post_card.widget.dart
+++ lib/widgets/posts/post_card.widget.dart
@@ -3,19 +3,29 @@ import 'package:varenya_mobile/models/post/post.model.dart';
 import 'package:varenya_mobile/widgets/posts/image_carousel.widget.dart';
 import 'package:varenya_mobile/widgets/posts/post_categories.widget.dart';
 import 'package:varenya_mobile/widgets/posts/post_user_details.widget.dart';
+import 'package:varenya_mobile/pages/post/post.page.dart' as PostPage;
 
 class PostCard extends StatelessWidget {
   final Post post;
+  final bool fullPagePost;
 
   PostCard({
     Key? key,
     required this.post,
+    this.fullPagePost = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: fullPagePost
+          ? () {}
+          : () {
+              Navigator.of(context).pushNamed(
+                PostPage.Post.routeName,
+                arguments: this.post.id,
+              );
+            },
       child: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
