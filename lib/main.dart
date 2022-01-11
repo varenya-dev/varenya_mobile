7b74fbabb6252e882a831a5edd0129f869425eab
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-11 07:44:25 +0530

    Post Category Hive Adapter Generated and Registered

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:varenya_mobile/app.dart';
 import 'package:provider/provider.dart';
 import 'package:varenya_mobile/constants/hive_boxes.constant.dart';
+import 'package:varenya_mobile/models/post/post_category/post_category.model.dart';
 import 'package:varenya_mobile/providers/user_provider.dart';
 import 'package:varenya_mobile/services/alerts_service.dart';
 import 'package:varenya_mobile/services/appointment.service.dart';
@@ -15,12 +16,15 @@ import 'package:varenya_mobile/services/doctor.service.dart';
 import 'package:varenya_mobile/services/post.service.dart';
 import 'package:varenya_mobile/services/user_service.dart';
 import 'package:varenya_mobile/utils/logger.util.dart';
+import 'package:hive/hive.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
   await Hive.initFlutter();
+
+  Hive.registerAdapter<PostCategory>(new PostCategoryAdapter());
 
   await Hive.openBox(VARENYA_DOCTORS_BOX);
   await Hive.openBox(VARENYA_POSTS_BOX);
