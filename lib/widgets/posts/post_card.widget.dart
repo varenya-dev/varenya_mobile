52bee7ae5404d4a39d7f8d23b88c2019bc32004f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-11 11:16:40 +0530

    Implementation for restricting certain features offline. Closes #114

--- lib/widgets/posts/post_card.widget.dart
+++ lib/widgets/posts/post_card.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:varenya_mobile/models/post/post.model.dart';
 import 'package:varenya_mobile/widgets/posts/image_carousel.widget.dart';
 import 'package:varenya_mobile/widgets/posts/post_categories.widget.dart';
@@ -17,15 +18,25 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: fullPagePost
-          ? () {}
-          : () {
-              Navigator.of(context).pushNamed(
-                PostPage.Post.routeName,
-                arguments: this.post.id,
-              );
-            },
+    return OfflineBuilder(
+      connectivityBuilder:
+          (BuildContext context, ConnectivityResult result, Widget child) {
+        final bool connected = result != ConnectivityResult.none;
+
+        return GestureDetector(
+          onTap: fullPagePost
+              ? null
+              : connected
+                  ? () {
+                      Navigator.of(context).pushNamed(
+                        PostPage.Post.routeName,
+                        arguments: this.post.id,
+                      );
+                    }
+                  : null,
+          child: child,
+        );
+      },
       child: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
