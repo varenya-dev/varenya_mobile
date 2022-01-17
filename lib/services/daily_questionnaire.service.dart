ac5f3b81a214f43ecf10be8377e3bfedf31577bb
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-17 08:11:26 +0530

    Service Implementation for retrieving data from device. Closes #124

--- lib/services/daily_questionnaire.service.dart
+++ lib/services/daily_questionnaire.service.dart
@@ -1,6 +1,13 @@
 import 'package:hive/hive.dart';
 import 'package:varenya_mobile/constants/hive_boxes.constant.dart';
+import 'package:varenya_mobile/models/daily_progress_data/daily_progress_data.model.dart';
 
 class DailyQuestionnaireService {
   final Box<List<dynamic>> _progressBox = Hive.box(VARENYA_PROGESS_BOX);
+
+  List<DailyProgressData> fetchDailyProgressData() {
+    return this
+        ._progressBox
+        .get(VARENYA_PROGESS_LIST, defaultValue: [])!.cast<DailyProgressData>();
+  }
 }
