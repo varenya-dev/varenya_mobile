ea9754ff7fb31df427cdb94833b1be4d54e5aa29
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-18 08:46:11 +0530

    UI Implementation for updating questions. Closes #133

--- lib/pages/daily_questionnaire/question.page.dart
+++ lib/pages/daily_questionnaire/question.page.dart
@@ -38,40 +38,6 @@ class _QuestionState extends State<Question> {
     this._questions = this._dailyQuestionnaireService.fetchDailyQuestions();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-
-    this._createQuestionController.dispose();
-    this._editQuestionController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Daily Questions Configuration'),
-      ),
-      body: ListView.builder(
-        itemCount: this._questions.length,
-        itemBuilder: (BuildContext context, int index) {
-          QuestionAnswer question = this._questions[index];
-
-          return QuestionDisplay(
-            question: question,
-            onEditQuestion: _handleCreateNewQuestion,
-            onDeleteQuestion: _handleCreateNewQuestion,
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.yellow,
-        onPressed: _handleCreateNewQuestion,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
   void _handleCreateNewQuestion() {
     showDialog(
       context: context,
@@ -128,5 +94,103 @@ class _QuestionState extends State<Question> {
     });
 
     Navigator.of(context).pop();
+  }
+
+  void _handleUpdateQuestion(QuestionAnswer question) {
+    this._editQuestionController.text = question.question;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        backgroundColor: Colors.grey[900],
+        title: Text('Update Question'),
+        content: Form(
+          key: this._editFormKey,
+          child: CustomFieldWidget(
+            textFieldController: this._editQuestionController,
+            label: 'Question',
+            validators: [
+              MinLengthValidator(
+                5,
+                errorText: 'Question should be at least 5 characters long',
+              ),
+            ],
+            textInputType: TextInputType.text,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              this._updateQuestion(question);
+            },
+            child: Text('Update'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _updateQuestion(QuestionAnswer question) {
+    if (!this._editFormKey.currentState!.validate()) {
+      return;
+    }
+
+    int questionIndex = this._questions.indexWhere(
+          (element) => element.id == question.id,
+        );
+
+    this._questions[questionIndex] = new QuestionAnswer(
+      id: uuid.v4(),
+      question: this._editQuestionController.text,
+      answer: '',
+    );
+
+    this._dailyQuestionnaireService.saveQuestions(this._questions);
+
+    setState(() {
+      this._questions = this._dailyQuestionnaireService.fetchDailyQuestions();
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    this._createQuestionController.dispose();
+    this._editQuestionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Questions Configuration'),
+      ),
+      body: ListView.builder(
+        itemCount: this._questions.length,
+        itemBuilder: (BuildContext context, int index) {
+          QuestionAnswer question = this._questions[index];
+
+          return QuestionDisplay(
+            question: question,
+            onEditQuestion: this._handleUpdateQuestion,
+            onDeleteQuestion: _handleCreateNewQuestion,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.yellow,
+        onPressed: _handleCreateNewQuestion,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
