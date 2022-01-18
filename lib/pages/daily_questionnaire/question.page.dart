6fd13612d1ec2a952add7a6b0fc0ccc297fd0a75
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-18 08:36:54 +0530

    UI Implementation for creation of questions. Closes #131

--- lib/pages/daily_questionnaire/question.page.dart
+++ lib/pages/daily_questionnaire/question.page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 import 'package:provider/provider.dart';
 import 'package:varenya_mobile/models/daily_progress_data/question_answer/question_answer.model.dart';
 import 'package:varenya_mobile/services/daily_questionnaire.service.dart';
+import 'package:varenya_mobile/widgets/common/custom_field_widget.dart';
 import 'package:varenya_mobile/widgets/daily_questionnaire/question_display.widget.dart';
+import 'package:uuid/uuid.dart';
 
 class Question extends StatefulWidget {
   const Question({Key? key}) : super(key: key);
@@ -17,12 +20,30 @@ class _QuestionState extends State<Question> {
   late final DailyQuestionnaireService _dailyQuestionnaireService;
   late List<QuestionAnswer> _questions;
 
+  final GlobalKey<FormState> _createFormKey = new GlobalKey<FormState>();
+  final TextEditingController _createQuestionController =
+      new TextEditingController();
+
+  final GlobalKey<FormState> _editFormKey = new GlobalKey<FormState>();
+  final TextEditingController _editQuestionController =
+      new TextEditingController();
+
+  final Uuid uuid = new Uuid();
+
   @override
   void initState() {
     super.initState();
 
     this._dailyQuestionnaireService = Provider.of(context, listen: false);
     this._questions = this._dailyQuestionnaireService.fetchDailyQuestions();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    this._createQuestionController.dispose();
+    this._editQuestionController.dispose();
   }
 
   @override
@@ -38,16 +59,74 @@ class _QuestionState extends State<Question> {
 
           return QuestionDisplay(
             question: question,
-            onEditQuestion: () {},
-            onDeleteQuestion: () {},
+            onEditQuestion: _handleCreateNewQuestion,
+            onDeleteQuestion: _handleCreateNewQuestion,
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.yellow,
-        onPressed: () {},
+        onPressed: _handleCreateNewQuestion,
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  void _handleCreateNewQuestion() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        backgroundColor: Colors.grey[900],
+        title: Text('Create New Question'),
+        content: Form(
+          key: this._createFormKey,
+          child: CustomFieldWidget(
+            textFieldController: this._createQuestionController,
+            label: 'Question',
+            validators: [
+              MinLengthValidator(
+                5,
+                errorText: 'Question should be at least 5 characters long',
+              ),
+            ],
+            textInputType: TextInputType.text,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: _createQuestion,
+            child: Text('Create'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _createQuestion() {
+    if (!this._createFormKey.currentState!.validate()) {
+      return;
+    }
+
+    this._questions.add(
+          new QuestionAnswer(
+            id: uuid.v4(),
+            question: this._createQuestionController.text,
+            answer: '',
+          ),
+        );
+
+    this._dailyQuestionnaireService.saveQuestions(this._questions);
+
+    setState(() {
+      this._questions = this._dailyQuestionnaireService.fetchDailyQuestions();
+    });
+
+    Navigator.of(context).pop();
   }
 }
