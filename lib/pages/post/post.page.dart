bc54a3ef0887f1c45e1da97d64731cfb88026275
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-30 19:00:08 +0530

    Post Page Cleanup

--- lib/pages/post/post.page.dart
+++ lib/pages/post/post.page.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:form_field_validator/form_field_validator.dart';
 import 'package:provider/provider.dart';
 import 'package:varenya_mobile/dtos/comments/create_comment/create_comment.dto.dart';
-import 'package:varenya_mobile/dtos/comments/delete_comment/delete_comment.dto.dart';
-import 'package:varenya_mobile/enum/comment_form_type.enum.dart';
-import 'package:varenya_mobile/enum/roles.enum.dart';
 import 'package:varenya_mobile/exceptions/auth/not_logged_in_exception.dart';
 import 'package:varenya_mobile/exceptions/server.exception.dart';
 import 'package:varenya_mobile/models/post/post.model.dart' as PM;
 import 'package:varenya_mobile/services/comments.service.dart';
 import 'package:varenya_mobile/services/post.service.dart';
 import 'package:varenya_mobile/utils/logger.util.dart';
-import 'package:varenya_mobile/utils/modal_bottom_sheet.dart';
 import 'package:varenya_mobile/utils/snackbar.dart';
 import 'package:varenya_mobile/widgets/comments/comment_form.widget.dart';
 import 'package:varenya_mobile/widgets/comments/comment_list.widget.dart';
-import 'package:varenya_mobile/widgets/common/custom_text_area.widget.dart';
-import 'package:varenya_mobile/widgets/common/profile_picture_widget.dart';
+import 'package:varenya_mobile/widgets/posts/full_post_body.widget.dart';
+import 'package:varenya_mobile/widgets/posts/full_post_duration.widget.dart';
+import 'package:varenya_mobile/widgets/posts/full_post_user_details.dart';
 import 'package:varenya_mobile/widgets/posts/image_carousel.widget.dart';
 
 class Post extends StatefulWidget {
@@ -78,90 +74,6 @@ class _PostState extends State<Post> {
     super.initState();
 
     this._postService = Provider.of<PostService>(context, listen: false);
-    this._commentsService =
-        Provider.of<CommentsService>(context, listen: false);
-  }
-
-  void _onDeleteComment(BuildContext context, String commentID) {
-    displayBottomSheet(
-      context,
-      Wrap(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text('Are you sure you want to delete this comment?'),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextButton(
-                onPressed: () async {
-                  await _confirmCommentDeletion(context, commentID);
-                },
-                child: Text('YES'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('NO'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _onUpdateComment(BuildContext context, PM.Post comment) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: Text('Update Comment'),
-        content: CommentForm(
-          commentFormType: CommentFormType.UPDATE,
-          refreshPost: () {
-            setState(() {});
-          },
-          postId: this._post!.id,
-          commentId: comment.id,
-          text: comment.body,
-        ),
-      ),
-    );
-  }
-
-  Future<void> _confirmCommentDeletion(
-      BuildContext context, String commentId) async {
-    try {
-      await this._commentsService.deleteComment(
-            new DeleteCommentDto(commentId: commentId),
-          );
-
-      setState(() {});
-
-      Navigator.of(context).pop();
-
-      displaySnackbar("Comment Deleted!", context);
-    } on ServerException catch (error) {
-      displaySnackbar(
-        error.message,
-        context,
-      );
-    } on NotLoggedInException catch (error) {
-      displaySnackbar(
-        error.message,
-        context,
-      );
-    } catch (error, stackTrace) {
-      log.e("PostPage:_confirmCommentDeletion", error, stackTrace);
-      displaySnackbar(
-        "Something went wrong, please try again later.",
-        context,
-      );
-    }
   }
 
   @override
@@ -172,83 +84,67 @@ class _PostState extends State<Post> {
 
     return Scaffold(
       bottomSheet: this._showCommentForm
-          ? Container(
-              color: Colors.transparent,
-              margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.03,
-                left: MediaQuery.of(context).size.width * 0.03,
-                right: MediaQuery.of(context).size.width * 0.03,
-              ),
-              child: Form(
-                key: this._formKey,
-                child: CustomTextArea(
-                  helperText: 'Type a comment...',
-                  textFieldController: this._commentController,
-                  validators: [
-                    RequiredValidator(
-                      errorText: 'Please enter some text',
-                    ),
-                  ],
-                  textInputType: TextInputType.text,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      Icons.chat,
-                      color: Colors.yellow,
-                    ),
-                    onPressed: this._onFormSubmit,
-                  ),
-                ),
-              ),
+          ? CommentForm(
+              refreshPost: () {
+                setState(() {});
+              },
+              postId: this._post!.id,
             )
           : null,
       appBar: AppBar(
         centerTitle: true,
         title: Text('Posts'),
       ),
-      body: FutureBuilder(
-        future: this._postService.fetchPostsById(this._postId!),
-        builder: (
-          BuildContext context,
-          AsyncSnapshot<PM.Post> snapshot,
-        ) {
-          if (snapshot.hasError) {
-            switch (snapshot.error.runtimeType) {
-              case ServerException:
-                {
-                  ServerException exception = snapshot.error as ServerException;
-                  return Text(exception.message);
-                }
-              default:
-                {
-                  log.e(
-                    "Post Error",
-                    snapshot.error,
-                    snapshot.stackTrace,
-                  );
-                  return Text("Something went wrong, please try again later");
-                }
-            }
-          }
-
-          if (snapshot.connectionState == ConnectionState.done) {
-            this._post = snapshot.data!;
-
-            return _buildBody();
-          }
-
-          return this._post == null
-              ? Column(
-                  children: [
-                    CircularProgressIndicator(),
-                  ],
-                )
-              : _buildBody();
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
         },
+        child: FutureBuilder(
+          future: this._postService.fetchPostsById(this._postId!),
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<PM.Post> snapshot,
+          ) {
+            if (snapshot.hasError) {
+              switch (snapshot.error.runtimeType) {
+                case ServerException:
+                  {
+                    ServerException exception =
+                        snapshot.error as ServerException;
+                    return Text(exception.message);
+                  }
+                default:
+                  {
+                    log.e(
+                      "Post Error",
+                      snapshot.error,
+                      snapshot.stackTrace,
+                    );
+                    return Text("Something went wrong, please try again later");
+                  }
+              }
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              this._post = snapshot.data!;
+
+              return _buildBody();
+            }
+
+            return this._post == null
+                ? Column(
+                    children: [
+                      CircularProgressIndicator(),
+                    ],
+                  )
+                : _buildBody();
+          },
+        ),
       ),
     );
   }
 
-  SingleChildScrollView _buildBody() {
+  Widget _buildBody() {
     Duration duration = _now.difference(this._post!.createdAt);
 
     return SingleChildScrollView(
@@ -257,92 +153,22 @@ class _PostState extends State<Post> {
         children: [
           Container(
             color: Colors.grey[900],
-            child: Row(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.01,
-                        vertical: MediaQuery.of(context).size.height * 0.01,
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: MediaQuery.of(context).size.width * 0.03,
-                        vertical: MediaQuery.of(context).size.height * 0.02,
-                      ),
-                      child: ProfilePictureWidget(
-                        imageUrl: this._post!.user.role == Roles.PROFESSIONAL
-                            ? this._post!.user.doctor!.imageUrl
-                            : '',
-                        size: MediaQuery.of(context).size.width * 0.1,
-                      ),
-                    ),
-                    Text(
-                      this._post!.user.role == Roles.PROFESSIONAL
-                          ? "Dr. ${this._post!.user.doctor!.fullName}"
-                          : this._post!.user.randomName!.randomName,
-                    )
-                  ],
-                ),
-              ],
+            child: FullPostUserDetails(
+              context: context,
+              post: _post,
             ),
           ),
           ImageCarousel(
             imageUrls: this._post!.images,
           ),
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.01,
-              vertical: MediaQuery.of(context).size.height * 0.01,
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.03,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  this._post!.user.role == Roles.PROFESSIONAL
-                      ? "Dr. ${this._post!.user.doctor!.fullName}"
-                      : this._post!.user.randomName!.randomName,
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-                if (duration.inSeconds < 60)
-                  Text(
-                    '${duration.inSeconds}s ago',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  )
-                else if (duration.inSeconds < 3600)
-                  Text(
-                    '${duration.inMinutes}m ago',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  )
-                else
-                  Text(
-                    '${duration.inHours}h ago',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  )
-              ],
-            ),
+          FullPostDuration(
+            context: context,
+            post: _post,
+            duration: duration,
           ),
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.01,
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.03,
-            ),
-            child: Text(
-              this._post!.body,
-            ),
+          FullPostBody(
+            context: context,
+            post: _post,
           ),
           Container(
             margin: EdgeInsets.symmetric(
@@ -362,8 +188,9 @@ class _PostState extends State<Post> {
           ),
           CommentList(
             comments: this._post!.comments,
-            onEditComment: this._onUpdateComment,
-            onDeleteComment: this._onDeleteComment,
+            refreshPost: () {
+              setState(() {});
+            },
           ),
         ],
       ),
