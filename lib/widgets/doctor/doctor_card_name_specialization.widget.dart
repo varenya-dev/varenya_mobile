99bcb0916e232ef19be378deb52aa4e161fe20f3
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-28 08:09:46 +0530

    Display Doctor's Name and Specialization Widget Implementation

---
+++ lib/widgets/doctor/doctor_card_name_specialization.widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:varenya_mobile/models/doctor/doctor.model.dart';
+
+class DoctorCardNameSpecialization extends StatelessWidget {
+  final Doctor doctor;
+
+  const DoctorCardNameSpecialization({
+    Key? key,
+    required this.doctor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.02,
+          ),
+          child: Text(
+            'Dr. ${this.doctor.fullName}',
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.height * 0.023,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.02,
+          ),
+          child: Text(
+            this.doctor.jobTitle,
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.height * 0.017,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
