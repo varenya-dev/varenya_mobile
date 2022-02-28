23eb46de64d67051150db70236db7cf43bdc286f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-28 08:10:00 +0530

    Display Doctor's Specialization Widget Implementation

---
+++ lib/widgets/doctor/display_specializations.widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:varenya_mobile/models/doctor/doctor.model.dart';
+import 'package:varenya_mobile/utils/palette.util.dart';
+
+class DisplaySpecializations extends StatelessWidget {
+  const DisplaySpecializations({
+    Key? key,
+    required this.doctor,
+  }) : super(key: key);
+
+  final Doctor doctor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.6,
+      height: MediaQuery.of(context).size.height * 0.06,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: this
+            .doctor
+            .specializations
+            .map((specialization) => Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.01,
+          ),
+          child: Chip(
+            backgroundColor: Palette.primary,
+            label: Text(
+              specialization.specialization,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ))
+            .toList(),
+      ),
+    );
+  }
+}
