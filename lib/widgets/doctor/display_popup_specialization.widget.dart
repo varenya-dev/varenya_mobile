c94c7d3a41ab81313643fe567221bf8ed96ec2cc
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-28 09:32:09 +0530

    Doctor Popup Specializations Widget Implementation

---
+++ lib/widgets/doctor/display_popup_specialization.widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:varenya_mobile/models/doctor/doctor.model.dart';
+import 'package:varenya_mobile/utils/palette.util.dart';
+
+class DisplayPopupSpecializations extends StatelessWidget {
+  const DisplayPopupSpecializations({
+    Key? key,
+    required this.doctor,
+  }) : super(key: key);
+
+  final Doctor doctor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.06,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: this
+            .doctor
+            .specializations
+            .map((specialization) => Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.01,
+          ),
+          child: Chip(
+            backgroundColor: Palette.primary,
+            label: Text(
+              specialization.specialization,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ))
+            .toList(),
+      ),
+    );
+  }
+}
